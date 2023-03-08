6bdf6912e965f6ba52f87ffe359f81802ddd3fe5
DenisChub <denis091201209@gmail.com>
2023-03-08 21:33:27 +0200

    first commit

---
+++ Fourth/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fourth
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            List<string> strings = new List<string>() { "один", "два", "три", "чотири", "п'ять" };
+
+            Func<string, int> stringLengthDelegate = s => s.Length;
+
+            foreach (var str in strings)
+            {
+                int length = stringLengthDelegate(str);
+                Console.WriteLine($"{str}: {length}");
+            }
+        }
+    }
+}
